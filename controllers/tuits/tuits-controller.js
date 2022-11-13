de786a41fe73dd2423b1d55a5243c6e7781f5bba
Mirza N Ahmed <ahmed.mir@husky.neu.edu>
2022-11-12 22:19:36 -0500

    first commit here

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,59 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime() + '';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+
+const findTuits  = (req, res) => {
+    // const type = req.query.type
+    // if(type) {
+    //     const tuitsOfType = tuits
+    //         .filter(t => t.type === type)
+    //     res.json(tuitsOfType)
+    //     return
+    // }
+    res.json(tuits);
+}
+
+
+const updateTuit = (req, res) => {
+    // const tuitId = req.params['tid'];
+    // const updates = req.body;
+    // tuits = tuits.map((twt) =>
+    //     twt._id === tuitId ?
+    //         {...twt, ...updates} :
+    //         twt
+    // );
+    // res.sendStatus(200);
+
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex(
+        (t) => t._id === tuitdIdToUpdate)
+    tuits[tuitIndex] =
+        {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+}
+
+
+const deleteTuit = (req, res) => {
+    const tuitIdToDelete = req.params['tid'];
+    tuits = tuits.filter((twt) =>
+        twt._id !== tuitIdToDelete);
+    res.sendStatus(200);
+}
+
+
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
